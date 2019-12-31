7986a3fcbf476f8914980fab850ac950bc71b116
fumei.jiang <fumei.jiang@code365.com>
2019-12-31 16:52:54 +0800

    config init

---
+++ config/src/test/java/com/git/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
