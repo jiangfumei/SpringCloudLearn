c1901a2a2d7036430ab26fd546dc52dfbf8d33e6
fumei.jiang <fumei.jiang@code365.com>
2020-01-02 14:48:00 +0800

    eureka 、provider、config

--- config/src/main/java/com/git/config/ConfigApplication.java
+++ config/src/main/java/com/git/config/ConfigApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/*@EnableConfigServer
-@EnableDiscoveryClient*/
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
 
