42132a7be3473ffd55ee3c748c0a71adf87f2bee
fumei.jiang <fumei.jiang@code365.com>
2020-01-06 10:05:06 +0800

    client pom add hystrixDashboard depemdency

--- client/src/main/java/com/eureka/client/ClientApplication.java
+++ client/src/main/java/com/eureka/client/ClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableZuulProxy
 @SpringBootApplication
 @RestController
+@EnableHystrixDashboard
 public class ClientApplication {
 
     public static void main(String[] args) {
