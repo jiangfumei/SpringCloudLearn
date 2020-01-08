c4d0d8736bb7597f427ccc277746f4690adf4676
fumei.jiang <fumei.jiang@code365.com>
2020-01-08 18:26:19 +0800

    predicate/pom.xml

---
+++ predicate/src/test/java/com/gateway/predicate/PredicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.predicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredicateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
