579ccd97d667d5fcd24464013a702f19e669db4d
fumei.jiang <fumei.jiang@code365.com>
2020-01-10 14:10:49 +0800

    ...

--- feign/src/main/java/com/client/feign/HiController.java
+++ feign/src/main/java/com/client/feign/HiController.java
@@ -1,6 +1,5 @@
 package com.client.feign;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
